a775b3b7a70fede752fc121c631992c53666328f
Eva Antonushkina <eant367@mail.ru>
2026-04-18 22:46:31 +0300

    fix: connect to db

--- app/db/database.py
+++ app/db/database.py
@@ -8,8 +8,14 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-  raise ValueError("DATABASE_URL is not set")
+    raise ValueError("DATABASE_URL is not set")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+DATABASE_URL = DATABASE_URL.strip()
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
